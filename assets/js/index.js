42d5841d9028cc55d31a3b235c2c18aed774a507
edsilv <e.silverton@gmail.com>
2014-02-09 00:03:00 +0000

    changed link colours

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,6 +10,13 @@
 
         $(".post-content").fitVids();
 
+        var em1 = "ed";
+        var em2 = "@";
+        var em3 = "edsilv.com";
+        var em4 = em1 + em2 + em3;
+
+        $(".mailto").prop('href', 'mailto:' + em4).prop('title', em4);
+
     });
 
 }(jQuery));
